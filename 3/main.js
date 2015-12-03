9289e581d5c64e1ed4266704c04f9a8a5eb163a1
Andreas Roth <art_github@rtline.de>
2015-12-03 11:58:59 +0100

    Adds broken step

--- 3/main.js
+++ 3/main.js
@@ -5,20 +5,16 @@ var startObject = ['0,0'];
 
 getInput(3).then((input) => {
 	var instructions = input.split('');
-	solveFirst(instructions);
-	solveSecond(instructions);
+
 });
 
 function solveFirst(instructions) {
-	var result = _.chain(instructions).reduce(addNewMove, ['0,0']).uniq().value().length;
-	console.log(`First result: ${result}`);
+	console.log(`First result: ${calcDistinctCoords(instructions).length}`);
 }
 
 function solveSecond(instructions) {
-	bothInstructions = _.partition(instructions, (elem, index) => index % 2 == 0);
-	result = _.uniq(_.chain(bothInstructions[0]).reduce(addNewMove, ['0,0']).uniq().value().concat(
-		_.chain(bothInstructions[1]).reduce(addNewMove, ['0,0']).uniq().value()
-	)).length;
+	var result = _.chain(instructions).partition((e, i) => i % 2 == 0)
+		.map(calcDistinctCoords).reduce((agg, arr) => agg.concat(arr)).uniq().value().length;
 	console.log(`Second result: ${result}`);
 }
 
@@ -29,4 +25,8 @@ function addNewMove(agg, move) {
 	if(move == '<') agg.push((p[0]-1) + ',' + p[1]);
 	if(move == '^') agg.push(p[0] + ',' + (p[1]-1));
 	return agg;
+}
+
+function calcDistinctCoords(instructions) {
+	return  _.chain(instructions).reduce(addNewMove, ['0,0']).uniq().value();
 }
