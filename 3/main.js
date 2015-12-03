f4e287a7ec8ca14e39783370d1c02dce12150cfb
Andreas Roth <art_github@rtline.de>
2015-12-03 13:16:58 +0100

    Final functional programming solution for 3

--- 3/main.js
+++ 3/main.js
@@ -1,32 +1,22 @@
 const getInput = require('../getInput');
 const _ = require('lodash');
 
-var startObject = ['0,0'];
-
 getInput(3).then((input) => {
-	var instructions = input.split('');
-
+  var result1 = calcDistinctCoords(input).length;
+  var result2 = _.chain(input).partition((e, i) => i % 2 == 0)
+		.map(calcDistinctCoords).union().flatten().uniq().value().length;
+	console.log(result1, result2);
 });
-
-function solveFirst(instructions) {
-	console.log(`First result: ${calcDistinctCoords(instructions).length}`);
-}
-
-function solveSecond(instructions) {
-	var result = _.chain(instructions).partition((e, i) => i % 2 == 0)
-		.map(calcDistinctCoords).reduce((agg, arr) => agg.concat(arr)).uniq().value().length;
-	console.log(`Second result: ${result}`);
-}
-
-function addNewMove(agg, move) {
-	var p = _.map(_.last(agg).split(','), (e) => parseInt(e));
-	if(move == '>') agg.push((p[0]+1) + ',' + p[1]);
-	if(move == 'v') agg.push(p[0] + ',' + (p[1]+1));
-	if(move == '<') agg.push((p[0]-1) + ',' + p[1]);
-	if(move == '^') agg.push(p[0] + ',' + (p[1]-1));
-	return agg;
-}
 
 function calcDistinctCoords(instructions) {
 	return  _.chain(instructions).reduce(addNewMove, ['0,0']).uniq().value();
+}
+
+function addNewMove(agg, move) {
+	var c = _.map(_.last(agg).split(','), (e) => parseInt(e));
+	if(move == '>') agg.push((c[0]+1) + ',' + c[1]);
+	if(move == 'v') agg.push(c[0] + ',' + (c[1]+1));
+	if(move == '<') agg.push((c[0]-1) + ',' + c[1]);
+	if(move == '^') agg.push(c[0] + ',' + (c[1]-1));
+	return agg;
 }
