9afc51b5eaa13f106bb99bfc0be70a16270aa798
Andreas Roth <art_github@rtline.de>
2015-12-03 18:11:55 +0100

    Small form correction

--- 3/main.js
+++ 3/main.js
@@ -9,8 +9,17 @@ var toCoordinates = _.wrap((x, y, move) => {
 	if(move == '^') return [x, y-1];
 }, (fn, coords, move) => fn(coords[0], coords[1], move));
 
-var result1 = _(input).scan(toCoordinates, [0,0]).uniq(false, c => c[0]+','+c[1]).value().length;
-var result2 = _(input).partition((e, i) => i % 2 == 0)
+var result1 = _(input)
+  .scan(toCoordinates, [0,0])
+  .uniq(false, c => c[0]+','+c[1])
+  .value().length;
+
+var result2 = _(input)
+  .partition((e, i) => i % 2 == 0)
 	.map((input) => _(input).scan(toCoordinates, [0,0]).value())
-  .union().flatten().uniq(false, c => c[0]+','+c[1]).value().length;
+  .union()
+  .flatten()
+  .uniq(false, c => c[0]+','+c[1])
+  .value().length;
+  
 console.log(result1, result2);
