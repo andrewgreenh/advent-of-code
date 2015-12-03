f70b66ffd0c1395f4a0d68efffde28b61fcbd261
Andreas Roth <art_github@rtline.de>
2015-12-03 13:57:11 +0100

    Adds small changes

--- 3/main.js
+++ 3/main.js
@@ -2,17 +2,14 @@ const getInput = require('../getInput');
 const _ = require('lodash');
 
 getInput(3).then((input) => {
-  var result1 = calcDistinctCoords(input).length;
-  var result2 = _.chain(input).partition((e, i) => i % 2 == 0)
-		.map(calcDistinctCoords).union().flatten().uniq().value().length;
+  var result1 = _.uniq(_(input).reduce(toCoordinates, ['0,0'])).length;
+  var result2 = _(input).partition((e, i) => i % 2 == 0)
+		.map((input) => _(input).reduce(toCoordinates, ['0,0']))
+    .union().flatten().uniq().value().length;
 	console.log(result1, result2);
 });
 
-function calcDistinctCoords(instructions) {
-	return  _.chain(instructions).reduce(addNewMove, ['0,0']).uniq().value();
-}
-
-function addNewMove(agg, move) {
+function toCoordinates(agg, move) {
 	var c = _.map(_.last(agg).split(','), (e) => parseInt(e));
 	if(move == '>') agg.push((c[0]+1) + ',' + c[1]);
 	if(move == 'v') agg.push(c[0] + ',' + (c[1]+1));
