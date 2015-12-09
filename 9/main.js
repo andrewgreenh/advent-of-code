a1171ef0477adf58f7b59fa788b6820ffc2cfe81
Andreas Roth <art_github@rtline.de>
2015-12-09 10:33:18 +0100

    Adds day 9

---
+++ 9/main.js
@@ -0,0 +1,13 @@
+const _ = require('lodash');
+const permutate = require('../permutate');
+var input = require('../getInput')(9).trim().split('\n');
+
+const grid = input.reduce((grid, row) => {
+  var [from,,to,,dist] = row.split(' ');
+  dist = parseInt(dist);
+  return _(grid).set( `${from}.${to}`, dist).set(`${to}.${from}`, dist).value();
+}, {});
+
+const dists = permutate(_.keys(grid)).map(perm => perm
+    .reduce((agg, e, i, perms) => agg+grid[perms[i]][perms[i-1]] || 0, 0));
+console.log(_.min(dists), _.max(dists));
