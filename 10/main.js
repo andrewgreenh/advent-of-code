0aeae1c947f9e8b6d5723b900a002c7c1333c319
Andreas Roth <art_github@rtline.de>
2015-12-10 09:21:32 +0100

    2line-conway

--- 10/main.js
+++ 10/main.js
@@ -1,8 +1,7 @@
-const _ = require('lodash');
-var input = require('../getInput')(10).trim();
+const input = require('../getInput')(10).trim();
 
-const result = _.range(50).reduce(
-  i => _(i.match(/(.)\1*/g)).map(d => d.length+d[0]).reduce((agg, s) => agg+s),
-  input);
+const
+  conwayStep = s => s.match(/(.)\1*/g).map(d => d.length+d[0]).join(''),
+  conway = (n, str) => new Array(n).fill().reduce(conwayStep, str);
 
-console.log(result.length);
+console.log(conway(40, input).length, conway(50, input).length);
