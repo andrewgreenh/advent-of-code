1c0e1bbc2c07139fcb10a8234fde4ab83b130db6
Andreas Roth <art_github@rtline.de>
2015-12-10 09:03:39 +0100

    Day 10 functional style

--- 10/main.js
+++ 10/main.js
@@ -1,22 +1,8 @@
 const _ = require('lodash');
 var input = require('../getInput')(10).trim();
 
-var result = input;
-_.times(40, () => result = parse(result));
+const result = _.range(50).reduce(
+  i => _(i.match(/(.)\1*/g)).map(d => d.length+d[0]).reduce((agg, s) => agg+s),
+  input);
 
 console.log(result.length);
-
-function parse(str) {
-  var done, result = '', rest = str;
-  while(!done) {
-    var startingDigits = rest.match(/^(\d)\1*/)[0];
-    if(startingDigits.length == rest.length) {
-      result += startingDigits.length + startingDigits[0];
-      done = true;
-    } else {
-      rest = rest.slice(startingDigits.length);
-      result += startingDigits.length + startingDigits[0];
-    }
-  }
-  return result;
-}
