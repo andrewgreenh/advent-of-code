21e18a33529959297c31b249b244fb831ea32483
Andreas Roth <art_github@rtline.de>
2015-12-14 15:27:43 +0100

    Finishes day 14

--- 14/main.js
+++ 14/main.js
@@ -1,22 +1,22 @@
 var input = require('../getInput')(14).trim().split('\n');
 var _ = require('lodash');
 
-var result = input.reduce((agg, line) => {
-  var obj = calc(line, 2503);
-  if(obj.distance > agg.distance) {
-    return obj;
-  }
-  return agg;
-}, {distance:0});
-
-function calc(str, n) {
-  var [name,,, speed,,, time,,,,,,, rest] = str.split(' ');
+const deers = input.reduce((result, line) => {
+  var [, name, speed, time, rest] = line.match(/([A-Z]\w+).* (\d+) .* (\d+) .* (\d+) /);
   var [speed, time, rest] = [speed, time, rest].map(_.ary(parseInt, 1));
-  var distance = Math.floor(n / (time + rest)) * speed * time;
-  var remaining = n % (time+rest);
-  if(remaining <= time) distance += remaining * speed;
-  else distance += time * speed;
-  return {name, distance};
-}
+  return _.set(result, name, {name, speed, time, rest, distance: 0, points: 0});
+}, {});
+const calc = (deer, n) => _.set(deer, 'distance',
+  Math.floor(n / (deer.time + deer.rest)) * deer.speed * deer.time +
+  deer.speed * Math.min(n % (deer.time + deer.rest), deer.time));
 
-console.log(result);
+_.range(1, 2504).forEach((i,e) => {
+  _.reduce(deers, (agg, deer) => {
+    deer = calc(deer, i);
+    if(deer.distance == agg[0].distance) return agg.concat([deer]);
+    return (deer.distance > agg[0].distance) ? [deer] : agg;
+  }, [{distance:0}]).forEach((i,e) => deers[i.name].points++);
+});
+
+console.log(_(deers).pluck('distance').max());
+console.log(_(deers).pluck('points').max());
