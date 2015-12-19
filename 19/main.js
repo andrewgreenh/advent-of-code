8b390028742e17a35a932a397ee460be0593ffda
Andreas Roth <art_github@rtline.de>
2015-12-19 10:46:19 +0100

    Small changes in day 19

--- 19/main.js
+++ 19/main.js
@@ -1,10 +1,14 @@
 const _ = require('lodash');
 var input = _.compact(require('../getInput')(19).trim().split('\n'));
 var medicine = input.pop();
-var result1 = _(input).map(transition => {
-  var [from, to] = transition.split(' => ');
-  return replacePossibilities(medicine, from, to);
-}).flatten().uniq().value();
+var transitions = input.map(line => ({
+  from: line.split(' => ')[0],
+  to: line.split(' => ')[1]
+}));
+var result1 = _(transitions)
+  .map(({from, to}) => replacePossibilities(medicine, from, to))
+  .flatten().uniq()
+  .value();
 var result2 = computeDistanceToE(medicine, input);
 console.log(result1.length, result2);
 
@@ -12,7 +16,7 @@ function replacePossibilities(haystack, needle, replace) {
   var rest = haystack;
   var results = [];
   var front = '';
-  while(rest.indexOf(needle) > -1) {
+  while(rest.includes(needle)) {
     var index = rest.indexOf(needle);
     front += rest.substring(0, index);
     var back = rest.substring(index+needle.length);
@@ -25,19 +29,19 @@ function replacePossibilities(haystack, needle, replace) {
 
 function computeDistanceToE(medicine, input) {
   var rest = medicine;
-  var count = 0;
-  var loop = true;
-  while(loop) {
-    var best = input.reduce((longest, trans) => {
+  var steps = 0;
+  var restIsGettingSmaller = true;
+  while(restIsGettingSmaller) {
+    var bestTransition = input.reduce((longest, trans) => {
       var [from, to] = trans.split(' => ');
       if(to.length < longest.to.length) return longest;
-      if(rest.indexOf(to) > -1) return {to, from};
+      if(rest.includes(to)) return {to, from};
       return longest;
     }, {from: '', to: ''});
-    var newRest = rest.replace(best.to, best.from);
+    var newRest = rest.replace(bestTransition.to, bestTransition.from);
     if(newRest == rest) break;
-    count++;
+    steps++;
     rest = newRest;
   }
-  return count;
+  return steps;
 }
