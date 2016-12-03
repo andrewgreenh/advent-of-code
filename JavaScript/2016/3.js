d0626eecde8e68cf3472af37486c3773a248c1a5
Andreas Roth <art_github@rtline.de>
2016-12-03 11:29:47 +0100

    Removes permute

--- JavaScript/2016/3.js
+++ JavaScript/2016/3.js
@@ -1,10 +1,9 @@
 const _ = require('lodash');
 const lines = _.compact(require('../getInput')(3, 2016).split('\n'));
-const permute = require('../permutate');
 
 const data = lines.map(line => _.compact(line.split(/\s+/)).map(i => parseInt(i, 10)));
 const data2 = _.flatMap(_.range(data.length / 3), i => _.times(3, j => _.times(3, k =>
   data[(i * 3) + k][j]
 )));
-const isPossibleTriangle = lengths => _.every(permute(lengths), o => (o[0] + o[1] > o[2]));
-console.log(_.filter(data, isPossibleTriangle).length, _.filter(data2, isPossibleTriangle).length);
+const isTriangle = ([a, b, c]) => a + b > c && a + c > b && c + b > a;
+console.log(_.filter(data, isTriangle).length, _.filter(data2, isTriangle).length);
