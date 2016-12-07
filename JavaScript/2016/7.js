9eeadba38d93ff3e78fb92de1340583449382164
Andreas Roth <art_github@rtline.de>
2016-12-07 11:42:16 +0100

    Improves on day 7 in JS

--- JavaScript/2016/7.js
+++ JavaScript/2016/7.js
@@ -2,44 +2,26 @@ const _ = require('lodash');
 require('../loadSlide');
 const lines = require('../getInput')(7, 2016).trim().split('\n');
 
-const isIp = s => {
-  let abbas = s.match(/(.)(.)\2\1/g);
-  if (abbas === null) return false;
-  abbas = _.reject(abbas, ([a, b, c, d]) => a === b);
-  const badAbbas = s.match(/\[[^[\]]*(.)(.)\2\1[^[\]]*]/g);
-  if (badAbbas !== null) return false;
-  if (_.isNull(abbas) || _.isEmpty(abbas)) return false;
-  return true;
+// Returns 2 strings for each line: One for the supernet sequence one for the hypernet sequence
+const parse = line => {
+  const parts = line.split(/\[([^[\]]+)]/);
+  return parts
+    .reduce(([arr1, arr2], item, index) =>
+      (index % 2 === 0 ? [[...arr1, item], arr2] : [arr1, [...arr2, item]]),
+      [[], []]
+    ).map(i => i.join(' - '));
 };
 
-const hasSSL = s => {
-  const supernetSequences = s.match(/\[[^[\]]*((.).\1)[^[\]]*]/g);
-  if (supernetSequences === null) return false;
-  const remaining = _.reduce(supernetSequences, (agg, seq) => agg.replace(seq, ''), s);
-  let abas = matchOverlap(remaining, /(.).\1/g);
-  if (abas === null) return false;
-  abas = _.reject(abas, ([a, b, c]) => a === b);
-  if (_.isEmpty(abas)) return false;
-  const expectedBabs = _.map(abas, ([a, b, c]) => `${b}${a}${b}`);
-  const hasMatching = _.some(supernetSequences, actualBab =>
-    _.some(expectedBabs, expectedBab =>
-      _.includes(actualBab, expectedBab)));
-  if (!hasMatching) return false;
-  return true;
+const ips = lines.map(parse);
+const hasAbba = s => {
+  const z = _.zip(s.split(''), s.slice(1).split(''), s.slice(2).split(''), s.slice(3).split(''));
+  return _.some(z, ([a, b, c, d]) => a !== b && a === d && b === c);
+};
+const canTLS = ([sn, hn]) => hasAbba(sn) && !hasAbba(hn);
+const canSSL = ([sn, hn]) => {
+  const z = _.zip(sn.split(''), sn.slice(1).split(''), sn.slice(2).split(''));
+  return _.some(z, ([a, b, c]) => a === c && a !== b && _.includes(hn, b + a + b));
 };
 
-console.log(lines.filter(isIp).length, lines.filter(hasSSL).length);
-
-function matchOverlap(input, re) {
-  const r = [];
-  let m;
-  if (!re.global) {
-    re = new RegExp(re.source, (re + '').split('/').pop() + 'g');
-  }
-  // eslint-disable-next-line
-  while (m = re.exec(input)) {
-    re.lastIndex -= m[0].length - 1;
-    r.push(m[0]);
-  }
-  return r;
-}
+console.log(ips.filter(canTLS).length);
+console.log(ips.filter(canSSL).length);
