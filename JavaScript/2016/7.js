7c36f663b87373593e0a8d7b4b38d5dbab522bd3
Andreas Roth <art_github@rtline.de>
2016-12-07 09:43:59 +0100

    Adds day 7 in js

--- JavaScript/2016/7.js
+++ JavaScript/2016/7.js
@@ -2,8 +2,6 @@ const _ = require('lodash');
 require('../loadSlide');
 const lines = require('../getInput')(7, 2016).trim().split('\n');
 
-const test = 'zazbz[bzb]cdb';
-
 const isIp = s => {
   let abbas = s.match(/(.)(.)\2\1/g);
   if (abbas === null) return false;
@@ -15,16 +13,33 @@ const isIp = s => {
 };
 
 const hasSSL = s => {
-  let abas = s.match(/(?=(.).\1)/g);
-  console.log(abas);
+  const supernetSequences = s.match(/\[[^[\]]*((.).\1)[^[\]]*]/g);
+  if (supernetSequences === null) return false;
+  const remaining = _.reduce(supernetSequences, (agg, seq) => agg.replace(seq, ''), s);
+  let abas = matchOverlap(remaining, /(.).\1/g);
   if (abas === null) return false;
   abas = _.reject(abas, ([a, b, c]) => a === b);
-  const actualBabs = s.match(/\[[^[\]]*(.).\1[^[\]]*]/g);
+  if (_.isEmpty(abas)) return false;
   const expectedBabs = _.map(abas, ([a, b, c]) => `${b}${a}${b}`);
-  const hasMatching = _.some(actualBabs, actualBab => _.some(expectedBabs, expectedBab => _.includes(actualBab, expectedBab)));
-  console.log(abas, actualBabs, expectedBabs);
+  const hasMatching = _.some(supernetSequences, actualBab =>
+    _.some(expectedBabs, expectedBab =>
+      _.includes(actualBab, expectedBab)));
   if (!hasMatching) return false;
   return true;
 };
 
-console.log(hasSSL(test));
+console.log(lines.filter(isIp).length, lines.filter(hasSSL).length);
+
+function matchOverlap(input, re) {
+  const r = [];
+  let m;
+  if (!re.global) {
+    re = new RegExp(re.source, (re + '').split('/').pop() + 'g');
+  }
+  // eslint-disable-next-line
+  while (m = re.exec(input)) {
+    re.lastIndex -= m[0].length - 1;
+    r.push(m[0]);
+  }
+  return r;
+}
