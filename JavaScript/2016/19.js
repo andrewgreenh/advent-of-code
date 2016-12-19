c572a67286e34000a0f96caa583fca18076bf175
Andreas Roth <art_github@rtline.de>
2016-12-19 10:10:49 +0100

    improves day 19

--- JavaScript/2016/19.js
+++ JavaScript/2016/19.js
@@ -1,50 +1,56 @@
-const _ = require('lodash');
 const input = +require('../getInput')(19, 2016).trim();
 
-function getElves(i) {
-  const elves = _.range(1, 1 + i).map((i, index, array) => new Node(i));
-  elves.forEach((node, index, array) => {
-    const nextNode = array[(index + 1) % array.length];
-    node.next = nextNode;
-    nextNode.prv = node;
-  });
-  return elves;
+class Node {
+  constructor(i) {
+    this.i = i;
+    this.next = this;
+    this.prev = this;
+  }
+
+  insert(node) {
+    const oldNext = this.next;
+    node.next = oldNext;
+    oldNext.prev = node;
+    this.next = node;
+    node.prev = this;
+    return node;
+  }
+
+  remove() {
+    this.prev.next = this.next;
+    this.next.prev = this.prev;
+    return this.next;
+  }
 }
 
-function Node(i) {
-  this.i = i;
-  this.next = null;
-  this.prv = null;
+function getElves(elfCount) {
+  let elf = new Node(1);
+  let i = elfCount - 1;
+  while (i--) elf = elf.insert(new Node(elfCount - i));
+  return elf.next;
 }
 
-Node.prototype.remove = function remove() {
-  this.prv.next = this.next;
-  this.next.prv = this.prv;
-};
+function solve1(elf) {
+  while (elf.next !== elf) elf = elf.next.remove();
+  console.log(elf);
+}
 
-function solve1() {
-  const elves = getElves(input);
-  let elf = elves[0];
-  while (elf.next !== elf) {
-    elf.next.remove();
+function solve2(elf) {
+  let midElf = getMiddleElf(elf, input);
+  let remainingElves = input;
+  while (remainingElves--) {
+    midElf = midElf.remove();
     elf = elf.next;
+    if (remainingElves % 2 === 0) midElf = midElf.next;
   }
   console.log(elf);
 }
 
-function solve2() {
-  const elves = getElves(input);
-  let elf = elves[0];
-  let midElf = elves[Math.floor(elves.length / 2)];
-  let remainingElves = elves.length;
-  while (elf.next !== elf) {
-    midElf.remove();
-    elf = elf.next;
-    midElf = remainingElves % 2 === 0 ? midElf.next : midElf.next.next;
-    remainingElves--;
-  }
-  console.log(elf);
+function getMiddleElf(elf, elfCount) {
+  let midIndex = Math.floor(input / 2);
+  while (midIndex--) elf = elf.next;
+  return elf;
 }
 
-solve1();
-solve2();
+solve1(getElves(input));
+solve2(getElves(input));
