4537de069fe0956a0e4452154bda7a7c42cd78fc
Andreas Roth <art_github@rtline.de>
2017-12-01 07:11:08 +0100

    removes lodash from day 1

--- JavaScript/2017/1.js
+++ JavaScript/2017/1.js
@@ -1,10 +1,10 @@
-const _ = require('lodash');
 const input = require('../getInput')(1, 2017).trim();
 
-const result = offset => input =>
-  _(input.split(''))
-    .map((val, index, arr) => (+val === +arr[(index + offset) % arr.length] ? +val : 0))
-    .sum();
+const result = offset => numberString =>
+  numberString
+    .split('')
+    .filter((val, index, arr) => val === arr[(index + offset) % arr.length])
+    .reduce((a, b) => a + +b, 0);
 
 console.log(result(1)(input));
 console.log(result(input.length / 2)(input));
