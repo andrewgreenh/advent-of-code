ed3ca6ed4501b63cdb8612324bb6a0e3a6a9dd11
Andreas Roth <art_github@rtline.de>
2017-12-02 15:35:39 +0100

    Starts with implementing more lib stuff

---
+++ TypeScript/src/lib/slide.ts
@@ -0,0 +1,17 @@
+import * as _ from 'lodash';
+
+function slide<T>(array: T[], windowSize: number): T[][] {
+  const result: T[][] = [];
+  for (let i = 0; i <= array.length - windowSize; i++) {
+    const window: T[] = [];
+    result.push(window);
+    for (let j = 0; j < windowSize; j++) {
+      window.push(array[i + j]);
+    }
+  }
+  return result;
+}
+
+_.mixin({ slide });
+
+export default slide;
