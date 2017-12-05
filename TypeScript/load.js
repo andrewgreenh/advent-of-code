dd460ad071986ab5a095b7c318c48f8ce5075d94
Andreas Roth <art_github@rtline.de>
2017-12-05 09:26:27 +0100

    Fixes wrong commit

--- TypeScript/load.js
+++ TypeScript/load.js
@@ -1,1 +1,33 @@
-null
+const fs = require('fs')
+const path = require('path')
+const request = require('request')
+
+const cookiePath = path.join(__dirname, '../sessionCookie.txt')
+const cookieString = fs.readFileSync(cookiePath, 'utf8')
+
+const day = process.argv[2]
+const year = process.argv[3]
+getInputOfDay(day, year)
+  .then(input => {
+    fs.writeFileSync(path.join(__dirname, `src/${year}/${day}.txt`), input)
+    console.log(`File written successfully: ${year}/${day}.txt`)
+  })
+  .catch(error => console.log(error))
+
+function getInputOfDay(day, year) {
+  const jar = request.jar()
+  const cookie = request.cookie(cookieString)
+  const url = `https://adventofcode.com/${year}/day/${day}/input`
+  jar.setCookie(cookie, url)
+  return new Promise((resolve, reject) => {
+    request.get({ url, jar }, (error, response, body) => {
+      if (response.statusCode === 200) {
+        resolve(body)
+      } else if (response.statusCode === 404) {
+        reject('Not ready')
+      } else {
+        throw 'Unknown error: ' + response.statusCode
+      }
+    })
+  })
+}
--- TypeScript/prepare.js
+++ TypeScript/prepare.js
@@ -4,18 +4,16 @@ const path = require('path')
 const day = process.argv[2]
 const year = process.argv[3]
 
-const content = `import * as _ from 'lodash';
-import getInput from '../lib/getInput';
+const content = `import getInput from '../lib/getInput'
+import { lines } from '../lib/ts-it/lines'
 
-const input = getInput(${+day}, ${+year}).trim();
+let result
+for (let line of lines(getInput(${+day}, ${year}))) {
+  console.log(line)
+}
 
-const parse = x => x;
+console.log(result)
 
-const result1 = _(input).thru(parse);
-// const result2 = _(input).thru(parse);
-
-console.log(result1);
-// console.log(result2);
 `
 
 fs.writeFileSync(path.join(__dirname, `src/${year}/${day}.ts`), content)
