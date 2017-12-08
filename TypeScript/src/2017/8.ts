4f76bf48b2c0e9b251a0ae410121ce97114dbb8b
Andreas Roth <art_github@rtline.de>
2017-12-08 06:37:59 +0100

    Finishes day 8

---
+++ TypeScript/src/2017/8.ts
@@ -0,0 +1,17 @@
+import getInput from '../lib/getInput'
+import { lines } from '../lib/ts-it/lines'
+import * as _ from 'lodash'
+
+let registers = {}
+let max = -Infinity
+let maxs = [0]
+for (let line of lines(getInput(8, 2017))) {
+  let [a, op, b, iff, reg, cond, numb] = line.split(' ')
+  if (!registers[a]) registers[a] = 0
+  if (!registers[reg]) registers[reg] = 0
+  eval(`if (registers.${reg} ${cond} ${numb}) registers.${a} ${op === 'inc' ? '+' : '-'}= ${b}`)
+  maxs.push(<number>_.max(<number[]>_.values(registers)))
+}
+
+console.log(_.max(<number[]>_.values(registers)))
+console.log(_.max(maxs))
