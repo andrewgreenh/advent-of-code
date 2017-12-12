00783407278ee4c6361ecac900d13c863aa6a5a7
Andreas Roth <art_github@rtline.de>
2017-12-12 06:51:43 +0100

    Implements day 12 with graphlib

---
+++ TypeScript/src/2017/12.ts
@@ -0,0 +1,14 @@
+import { alg, Graph } from 'graphlib'
+
+import getInput from '../lib/getInput'
+import { lines } from '../lib/ts-it/lines'
+
+let graph = new Graph({ directed: false })
+for (let line of lines(getInput(12, 2017))) {
+  let [group, others] = line.split('<->').map(x => x.trim())
+  for (let other of others.split(', ')) graph.setEdge(group, other)
+}
+
+let components = alg.components(graph)
+console.log((<any[]>components.find(c => c.includes('0'))).length)
+console.log(components.length)
