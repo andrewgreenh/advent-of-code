b95087fafb4c55aa6b84ee9c161e8263803aa3f6
Andreas Roth <art_github@rtline.de>
2017-12-16 16:12:09 +0100

    Improves day 16

--- TypeScript/src/2017/16.ts
+++ TypeScript/src/2017/16.ts
@@ -1,53 +1,22 @@
 import getInput from '../lib/getInput'
-import { enumerate } from '../lib/ts-it/enumerate'
-import { range } from '../lib/ts-it/range'
 
-function danceN(n) {
-  let start = 'abcdefghijklmnop'
-  let programs = start.split('')
-  let moves = getInput(16, 2017).split(',')
-
-  let history: string[] = [start]
-  let cycleLength = 0
-  for (cycleLength of range(0, n)) {
-    dance()
-    let string = programs.join('')
-    if (history.includes(string)) break
-    else history.push(string)
-  }
-  if (cycleLength > 0) for (let i of range(0, n % (cycleLength + 1))) dance()
-  return programs.join('')
-
-  function dance() {
-    for (let move of moves) {
-      if (move[0] === 's') spin(+move.slice(1))
-      if (move[0] === 'p') partner(move[1], move[3])
-      if (move[0] === 'x') {
-        let [ia, ib] = move
-          .slice(1)
-          .split('/')
-          .map(Number) as [number, number]
-        exchange(ia, ib)
-      }
-    }
-  }
-
-  function spin(n) {
-    let toMove = programs.slice(-n)
-    for (let i = programs.length - n - 1; i >= 0; i--) programs[i + n] = programs[i]
-    for (let [index, char] of enumerate(toMove)) programs[index] = char
-  }
-
-  function exchange(ia, ib) {
-    let temp = programs[ia]
-    programs[ia] = programs[ib]
-    programs[ib] = temp
-  }
-
-  function partner(a, b) {
-    exchange(programs.indexOf(a), programs.indexOf(b))
-  }
+let moveMap = {
+  s: n => p => [...p.slice(-+n), ...p.slice(0, -+n)],
+  p: (a, b) => p => p.map(x => (x === a ? b : x === b ? a : x)),
+  x: (a, b) => p => p.map((x, i) => (i === +a ? p[+b] : i === +b ? p[+a] : x)),
 }
 
-console.log(danceN(1))
-console.log(danceN(1000000000))
+let input = getInput(16, 2017).trim()
+let moves = input.split(',').map(move => moveMap[move[0]](...move.slice(1).split('/')))
+let dance = p => moves.reduce((p, move) => move(p), p.split('')).join('')
+
+const danceN = (n, p, history: string[] = []) => {
+  for (let cycleLength = 0; cycleLength < n; cycleLength++, p = dance(p)) {
+    if (history.includes(p)) return history[n % cycleLength]
+    history.push(p)
+  }
+  return p
+}
+
+console.log(dance('abcdefghijklmnop'))
+console.log(danceN(1000000000, 'abcdefghijklmnop'))
