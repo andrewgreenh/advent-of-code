9bb7894121ee688d6d14174213153db1d55f6939
Andreas Roth <art_github@rtline.de>
2018-12-23 18:39:17 +0100

    Finally complete 22 2018 correctly

--- TypeScript/src/2018/22.ts
+++ TypeScript/src/2018/22.ts
@@ -6,84 +6,43 @@ import { range } from '../lib/ts-it/range';
 
 const strings = [...lines(getInput(22, 2018))];
 const depth = numbers(strings[0])[0];
-const target = numbers(strings[1]);
-const start = [0, 0];
+const end = [...numbers(strings[1]), 1];
+const start = [0, 0, 1];
+const key = ([a, b]: number[]) => `${a}%${b}`;
 
-const cache = {
-  [key(start)]: 0 + (depth % 20183),
-  [key(target)]: 0 + (depth % 20183),
-};
+const cache: ObjectOf<number> = {};
 function getLevel(pos: number[]) {
   const k = key(pos);
   if (cache[k] !== undefined) return cache[k];
   const [x, y] = pos;
   let index = -1;
-  if (y === 0) index = x * 16807;
+  if (y === 0 && x === 0) index = 0;
+  else if (y === 0) index = x * 16807;
   else if (x === 0) index = y * 48271;
   else index = getLevel([x - 1, y]) * getLevel([x, y - 1]);
   cache[k] = (index + depth) % 20183;
   return cache[k];
 }
-function risk(pos: number[]) {
-  return getLevel(pos) % 3;
-}
-
-function key([a, b]: number[]) {
-  return `${a}%${b}`;
-}
-
+const risk = (pos: number[]) => getLevel(pos) % 3;
 let sum = 0;
-for (const y of range(0, target[1] + 1))
-  for (const x of range(0, target[0] + 1)) sum += risk([x, y]);
+for (const y of range(0, end[1] + 1))
+  for (const x of range(0, end[0] + 1)) sum += risk([x, y]);
 console.log(sum);
 
-type Node = {
-  stuff: 'climbing-gear' | 'torch' | 'neither';
-  pos: number[];
-};
-
-const hashNode = (node: Node) => `${node.stuff}%${key(node.pos)}`;
-const path = aStar<Node>({
-  getNeighbourCost: (a, b) => (a.stuff === b.stuff ? 1 : 7),
-  getNeighbours: node => {
-    return [...possiblePossitions(node), possibleEquipment(node)];
-  },
-  hashData: n => hashNode(n),
-  isEnd: node => key(node.pos) === key(target) && node.stuff === 'torch',
-  startNode: { stuff: 'torch', pos: start },
+const hashNode = (node: number[]) => node.join('---');
+const abs = Math.abs;
+const path = aStar<number[]>({
+  estimateCost: d => abs(d[0] - end[0]) + abs(d[1] - end[1]),
+  getNeighbourCost: (a, b) => (a[2] === b[2] ? 1 : 7),
+  getNeighbours: ([x, y, t]) => [
+    [x, y, [0, 1, 2].find(i => i !== t && i !== risk([x, y]))!],
+    ...[[x + 1, y, t], [x, y + 1, t], [x - 1, y, t], [x, y - 1, t]].filter(
+      ([x, y, t]) => x >= 0 && y >= 0 && risk([x, y]) !== t,
+    ),
+  ],
+  hashData: hashNode,
+  isEnd: node => hashNode(node) === hashNode(end),
+  startNode: start,
 });
 
 console.log(path.isSuccess() && path.cost);
-
-function possibleEquipment(node: Node): Node {
-  if (risk(node.pos) === 0) {
-    return {
-      pos: node.pos,
-      stuff: node.stuff === 'torch' ? 'climbing-gear' : 'torch',
-    };
-  }
-  if (risk(node.pos) === 1) {
-    return {
-      pos: node.pos,
-      stuff: node.stuff === 'neither' ? 'climbing-gear' : 'neither',
-    };
-  }
-  return {
-    pos: node.pos,
-    stuff: node.stuff === 'torch' ? 'neither' : 'torch',
-  };
-}
-
-function possiblePossitions(node: Node): Node[] {
-  const [x, y] = node.pos;
-  const candidatedPositions = [[x + 1, y], [x, y + 1], [x - 1, y], [x, y - 1]];
-  return candidatedPositions.map(pos => ({ ...node, pos })).filter(isLegal);
-}
-
-function isLegal(node: Node) {
-  if (node.pos[0] < 0 || node.pos[1] < 0) return false;
-  if (risk(node.pos) === 0 && node.stuff === 'neither') return false;
-  if (risk(node.pos) === 1 && node.stuff === 'torch') return false;
-  if (risk(node.pos) === 2 && node.stuff === 'climbing-gear') return false;
-  return true;
-}
