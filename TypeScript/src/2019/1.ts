578aad5d3dcaadcffbd61d92529f72c7d8b65b33
Andreas Roth <art_github@rtline.de>
2019-12-01 06:59:59 +0100

    Rework to 2019 1 to simple for loop solution

--- TypeScript/src/2019/1.ts
+++ TypeScript/src/2019/1.ts
@@ -1,19 +1,20 @@
 import getInput from '../lib/getInput';
+import { log } from '../lib/log';
 import { floor } from '../lib/math/floor';
 import { iterable } from '../lib/ts-it/iterable';
 import { lines as stringToLines } from '../lib/ts-it/lines';
-import { map } from '../lib/ts-it/map';
-import { pipe } from '../lib/ts-it/pipe';
-import { sum } from '../lib/ts-it/sum';
 
 const input = getInput(1, 2019);
 const lines = iterable(() => stringToLines(input));
 
-let baseCalc = (n: number) => floor(n / 3) - 2;
-let recursive = (n: number): number =>
-  baseCalc(n) < 0 ? 0 : baseCalc(n) + recursive(baseCalc(n));
-
-let solve = (calc: typeof baseCalc) => pipe(lines)(map(Number), map(calc), sum);
-
-console.log(solve(baseCalc));
-console.log(solve(recursive));
+let resA = 0;
+let resB = 0;
+for (let l of lines) {
+  let f = floor(+l / 3) - 2;
+  resA += f;
+  while (f > 0) {
+    resB += f;
+    f = floor(f / 3) - 2;
+  }
+}
+log(resA, resB);
