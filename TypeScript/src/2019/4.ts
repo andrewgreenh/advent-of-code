8b1462b054f889629d270898aa545b00ab394036
Andreas Roth <art_github@rtline.de>
2019-12-04 22:16:54 +0100

    Improve day 4

--- TypeScript/src/2019/4.ts
+++ TypeScript/src/2019/4.ts
@@ -1,43 +1,22 @@
 import getInput from '../lib/getInput';
+import { str } from '../lib/str';
 import { any } from '../lib/ts-it/any';
 import { countBy } from '../lib/ts-it/countBy';
-import { every } from '../lib/ts-it/every';
-import { filter } from '../lib/ts-it/filter';
-import { map } from '../lib/ts-it/map';
-import { p } from '../lib/ts-it/pipe';
+import { pipe } from '../lib/ts-it/pipe';
 import { range } from '../lib/ts-it/range';
-import slide from '../lib/ts-it/slide';
-import { tap } from '../lib/ts-it/tap';
-import { toArray } from '../lib/ts-it/toArray';
 import { values } from '../lib/utils';
 
 const input = getInput(4, 2019);
 const [start, end] = input.split('-').map(Number);
 
-p(range(start, end))(
-  map(x => [...x.toString()]),
-  filter(s => {
-    const doubles = p(s)(
-      countBy(),
-      values,
-      any(x => x >= 2),
-    );
-    const monotone = p(s)(
-      slide(2),
-      every(([a, b]) => +a <= +b),
-    );
-    return !!doubles && monotone;
-  }),
-  toArray,
-  tap(a => console.log(a.length)),
-  filter(n => {
-    const onlyDoubles = p(n.toString())(
-      countBy(),
-      values,
-      any(x => x === 2),
-    );
-    return onlyDoubles;
-  }),
-  toArray,
-  tap(a => console.log(a.length)),
-);
+let [a, b] = [0, 0];
+for (let x of range(start, end)) {
+  let s = [...str(x)];
+  if (s.join('') !== s.sort().join('')) continue;
+  let c = pipe(s)(countBy(), values);
+  if (!pipe(c)(any(x => x >= 2))) continue;
+  a++;
+  if (!pipe(c)(any(x => x === 2))) continue;
+  b++;
+}
+console.log(a, b);
