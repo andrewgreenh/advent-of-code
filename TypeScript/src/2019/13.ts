11e66f9a32f6de632c8185fc7c5bf34ff04f898e
andreasroth <andreas.roth@xent.de>
2019-12-13 09:07:33 +0100

    Small changes to 13 2019

--- TypeScript/src/2019/13.ts
+++ TypeScript/src/2019/13.ts
@@ -1,46 +1,22 @@
 import getInput from '../lib/getInput';
 import { IntCodeComputer, IntCodeScreen } from '../lib/intCode';
-import { countIf } from '../lib/ts-it/countIf';
-import { flatten } from '../lib/ts-it/flatten';
 import { numbers } from '../lib/ts-it/numbers';
-import { p } from '../lib/ts-it/pipe';
 
 const input = getInput(13, 2019);
 const ins = numbers(input);
 
-let c = new IntCodeComputer([...ins], paint1);
-
-let s = new IntCodeScreen();
-let offset = 0;
-let x = -1;
-let y = -1;
-function paint1(n: number) {
-  if (offset === 0) x = n;
-  if (offset === 1) y = n;
-  if (offset === 2) {
-    if (n === 0) s.grid.set([x, y], ' ');
-    if (n === 1) s.grid.set([x, y], '#');
-    if (n === 2) s.grid.set([x, y], 'x');
-    if (n === 3) s.grid.set([x, y], '-');
-    if (n === 4) s.grid.set([x, y], 'o');
-  }
-  offset = (offset + 1) % 3;
-}
-c.run();
-
-const part1 = p(s.grid.toGrid())(
-  flatten,
-  countIf(x => x === 2),
-);
+let p1 = 0;
+let c1 = new IntCodeComputer([...ins], x => (p1 += x === 2 ? 1 : 0));
+c1.run();
 
 const ins2 = [...ins];
 ins2[0] = 2;
-c = new IntCodeComputer(ins2, paint2);
-s.grid.clear();
+let c2 = new IntCodeComputer(ins2, paint2);
+let s = new IntCodeScreen();
 
-offset = 0;
-x = -1;
-y = -1;
+let offset = 0;
+let x = -1;
+let y = -1;
 let score = 0;
 let ballX = -1;
 let paddleX = -1;
@@ -65,15 +41,15 @@ function paint2(n: number) {
 
 async function play() {
   try {
-    c.run();
-    console.log(part1);
+    c2.run();
+    console.log(p1);
     console.log(score);
   } catch (e) {
     s.paint(true);
-    await new Promise(r => setTimeout(r, 150));
-    if (ballX > paddleX) c.addInput(1);
-    if (ballX < paddleX) c.addInput(-1);
-    if (ballX === paddleX) c.addInput(0);
+    await new Promise(r => setTimeout(r, 16));
+    if (ballX > paddleX) c2.addInput(1);
+    if (ballX < paddleX) c2.addInput(-1);
+    if (ballX === paddleX) c2.addInput(0);
     play();
   }
 }
