d076bcdde25532e875a9b9044a9f020b020dbb39
andreasroth <github@xent.de>
2020-12-10 06:54:56 +0100

    shorten day 1

--- TypeScript/src/2020/10.ts
+++ TypeScript/src/2020/10.ts
@@ -1,25 +1,19 @@
 import { max, memoize, range, sortBy } from 'lodash';
 import getInput from '../lib/getInput';
 import { stringToLines } from '../lib/ts-it/lines';
+import { p } from '../lib/ts-it/pipe';
+import slide from '../lib/ts-it/slide';
 
 const input = getInput(10, 2020);
 const lines = stringToLines(input).map(Number);
 
-const nums = sortBy(lines);
-const m = max(nums)! + 3;
-let last = 0;
-nums.push(m);
-nums.unshift(0);
+let nums = sortBy(lines);
+nums = [0, ...nums, max(nums)! + 3];
+
 let ones = 0;
 let threes = 0;
-
-for (const i of nums) {
-  let diff = i - last;
-  if (diff === 1) ones++;
-  if (diff === 3) threes++;
-  last = i;
-}
-
+for (const [a, b] of p(nums)(slide(2)))
+  b - a === 1 ? ones++ : b - a === 3 ? threes++ : 0;
 console.log(ones * threes);
 
 const count = memoize((t: number, x = 0): number => {
@@ -27,5 +21,4 @@ const count = memoize((t: number, x = 0): number => {
   for (let s of range(t - 3, t)) if (nums[t] - nums[s] <= 3) x += count(s);
   return x;
 });
-
 console.log(count(nums.length - 1));
