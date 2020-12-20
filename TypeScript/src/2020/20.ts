72f2cfac142968e52dda3c18142e5decf5d63f3a
andreasroth <github@xent.de>
2020-12-20 09:14:03 +0100

    Add part 2 of 20 2020

--- TypeScript/src/2020/20.ts
+++ TypeScript/src/2020/20.ts
@@ -40,10 +40,6 @@ const allRotations = memoize(function allRotations(tile: string[][]) {
   return result;
 });
 
-// for (const t of allRotations(parts[0][1])) {
-//   printGrid(t, '', '');
-// }
-
 function doesFit(
   fixedTiles: (Part | null)[][],
   nextX: number,
@@ -69,22 +65,26 @@ function doesFit(
   return topFits && leftFits;
 }
 
-function printGridOfGrids(fixedTiles: (Part | null)[][]) {
-  for (const y of range(0, side * parts[0][1].length)) {
-    if (y % parts[0][1].length === 0) console.log('\n');
+function printGridOfGrids(fixedTiles: (Part | null)[][], spaces = true) {
+  const sampleTile = fixedTiles[0][0]?.[1];
+  if (!sampleTile) return;
+  let rows = '';
+  for (const y of range(0, fixedTiles.length * sampleTile.length)) {
     let row = '';
-    for (const x of range(0, side * parts[0][1][0].length)) {
-      if (x % parts[0][1][0].length === 0) row += ' ';
-      const tileX = Math.floor(x / parts[0][1][0].length);
-      const tileY = Math.floor(y / parts[0][1].length);
+    if (y % sampleTile.length === 0 && spaces) row += '\n';
+    for (const x of range(0, fixedTiles[0].length * sampleTile[0].length)) {
+      if (x % sampleTile[0].length === 0 && spaces) row += ' ';
+      const tileX = Math.floor(x / sampleTile[0].length);
+      const tileY = Math.floor(y / sampleTile.length);
       const tile = fixedTiles[tileY][tileX]?.[1];
-      const innerX = x % parts[0][1][0].length;
-      const innerY = y % parts[0][1].length;
+      const innerX = x % sampleTile[0].length;
+      const innerY = y % sampleTile[0].length;
       const char = tile?.[innerY]?.[innerX] ?? '';
       row += char;
     }
-    console.log(row);
+    rows += row + '\n';
   }
+  console.log(rows);
 }
 
 function* findSolution(
@@ -118,7 +118,85 @@ for (const x of findSolution(emptyResult, 0)) {
     s *= row[0]![0];
     s *= row[row.length - 1]![0];
   }
-  console.log(s);
   printGridOfGrids(x);
+  console.log(s);
+
+  const partsWithoutBorders = x.map((rowOfParts) =>
+    rowOfParts.map(
+      (part): Part => [
+        part![0],
+        part![1].slice(1, -1).map((row) => row.slice(1, -1)),
+      ],
+    ),
+  );
+
+  printGridOfGrids(partsWithoutBorders, false);
+
+  const baseNessy = `..................#.
+#....##....##....###
+.#..#..#..#..#..#...`
+    .split('\n')
+    .map((l) => l.split(''));
+
+  const allNessies = allRotations(baseNessy);
+
+  const sampleTile = partsWithoutBorders[0][0][1];
+
+  const maxY = sampleTile.length * partsWithoutBorders.length;
+  const maxX = sampleTile[0].length * partsWithoutBorders[0].length;
+
+  for (const nessy of allNessies) {
+    for (let y of range(0, maxY - nessy.length)) {
+      for (let x of range(0, maxX - nessy[0].length)) {
+        let matches = true;
+        for (let dy of range(0, nessy.length)) {
+          for (let dx of range(0, nessy[0].length)) {
+            const totalX = x + dx;
+            const totalY = y + dy;
+            const tileX = Math.floor(totalX / sampleTile[0].length);
+            const tileY = Math.floor(totalY / sampleTile.length);
+            const localX = totalX % sampleTile[0].length;
+            const localY = totalY % sampleTile.length;
+
+            if (
+              partsWithoutBorders[tileY][tileX][1][localY][localX] === '.' &&
+              nessy[dy][dx] === '#'
+            )
+              matches = false;
+          }
+        }
+        if (matches) {
+          for (let dy of range(0, nessy.length)) {
+            for (let dx of range(0, nessy[0].length)) {
+              const totalX = x + dx;
+              const totalY = y + dy;
+              const tileX = Math.floor(totalX / sampleTile[0].length);
+              const tileY = Math.floor(totalY / sampleTile.length);
+              const localX = totalX % sampleTile[0].length;
+              const localY = totalY % sampleTile.length;
+
+              if (nessy[dy][dx] === '#')
+                partsWithoutBorders[tileY][tileX][1][localY][localX] = 'O';
+            }
+          }
+        }
+      }
+    }
+  }
+  printGridOfGrids(partsWithoutBorders, false);
+
+  let s2 = 0;
+  for (let partRow of partsWithoutBorders) {
+    for (let part of partRow) {
+      for (let tileRow of part[1]) {
+        for (let c of tileRow) {
+          if (c === '#') s2++;
+        }
+      }
+    }
+  }
+
+  console.log(s2);
+
   break;
 }
