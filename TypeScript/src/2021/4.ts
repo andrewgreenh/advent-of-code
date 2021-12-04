907dede780142119bd1672324738629e7e1f48e2
Andreas Roth <andreas.roth@esveo.com>
2021-12-04 07:19:19 +0100

    Finish day 4 and prepare day 5

--- TypeScript/src/2021/4.ts
+++ TypeScript/src/2021/4.ts
@@ -1,15 +1,37 @@
-import getInput from '../lib/getInput'
-import { stringToLines } from '../lib/ts-it/lines';
+import getInput from '../lib/getInput';
+import { enumerate } from '../lib/ts-it/enumerate';
+import { last } from '../lib/ts-it/last';
 import { numbers } from '../lib/ts-it/numbers';
+import { p } from '../lib/ts-it/pipe';
+import { range } from '../lib/ts-it/range';
+import { sumBy } from '../lib/ts-it/sumBy';
 
-const input = getInput(4, 2021);
-const nums = numbers(input);
-const lines = stringToLines(input);
+const [[header], ...boards] = getInput(4, 2021)
+  .split('\n\n')
+  .map((part) => part.split('\n').map(numbers));
 
-let result
-for (const line of lines) {
-  console.log(line)
+const winners: typeof boards = [];
+const results: number[] = [];
+
+for (const num of header) {
+  for (const board of boards) {
+    if (winners.includes(board)) continue;
+    for (const [y, row] of enumerate(board)) {
+      for (const [x, cell] of enumerate(row)) {
+        if (cell === num) board[y][x] = -num;
+      }
+    }
+
+    for (const n of range(0, 5)) {
+      if (board[n].every((x) => x < 0)) handleWinner(board, num);
+      if (board[0].every((v, i) => board[i][n] < 0)) handleWinner(board, num);
+    }
+  }
 }
+console.log(results[0], last(results));
 
-console.log(result)
-
+function handleWinner(board: number[][], num: number) {
+  winners.push(board);
+  const result = p(board.flat())(sumBy((x) => (x < 0 ? 0 : x)));
+  results.push(result * num);
+}
