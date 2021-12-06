1d4917c7e47906cc0c88b53637b16d6ca763665e
Andreas Roth <andreas.roth@esveo.com>
2021-12-06 06:22:42 +0100

    FInish day 6 and prepare day 7

--- TypeScript/src/2021/6.ts
+++ TypeScript/src/2021/6.ts
@@ -1,15 +1,21 @@
-import getInput from '../lib/getInput'
-import { stringToLines } from '../lib/ts-it/lines';
+import { DefaultArray } from '../lib/DefaultArray';
+import getInput from '../lib/getInput';
 import { numbers } from '../lib/ts-it/numbers';
+import { sum } from '../lib/ts-it/sum';
 
 const input = getInput(6, 2021);
-const nums = numbers(input);
-const lines = stringToLines(input);
+const fishes = numbers(input);
 
-let result
-for (const line of lines) {
-  console.log(line)
+const fishesWithNumber = DefaultArray(() => 0);
+for (const fish of fishes) fishesWithNumber[fish]++;
+
+for (let n = 0; n < 256; n++) {
+  let toAdd = 0;
+  for (let i = 0; i <= 8; i++) {
+    if (i === 0) toAdd = fishesWithNumber[i];
+    if (i > 0) fishesWithNumber[i - 1] = fishesWithNumber[i];
+  }
+  fishesWithNumber[6] += toAdd;
+  fishesWithNumber[8] = toAdd;
+  if (n === 255 || n === 79) console.log(sum(fishesWithNumber));
 }
-
-console.log(result)
-
