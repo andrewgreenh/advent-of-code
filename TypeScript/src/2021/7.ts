21d2cc4970c4a92202da9242dae623f84b231a67
Andreas Roth <andreas.roth@esveo.com>
2021-12-07 08:06:27 +0100

    Add day 7 2021 and prepare 8

--- TypeScript/src/2021/7.ts
+++ TypeScript/src/2021/7.ts
@@ -1,15 +1,22 @@
-import getInput from '../lib/getInput'
-import { stringToLines } from '../lib/ts-it/lines';
+import { max, min, minBy, range, sumBy } from 'lodash';
+import getInput from '../lib/getInput';
+import { abs } from '../lib/math/abs';
+import { sumUntil } from '../lib/math/sumUntil';
 import { numbers } from '../lib/ts-it/numbers';
 
 const input = getInput(7, 2021);
 const nums = numbers(input);
-const lines = stringToLines(input);
 
-let result
-for (const line of lines) {
-  console.log(line)
-}
+let minMove = -1;
+let minCount = Infinity;
 
-console.log(result)
-
+const sortedNums = range(min(nums)!, max(nums)! + 1);
+const part1 = minBy(
+  sortedNums.map((i) => ({ i, e: sumBy(nums, (n) => abs(n - i)) })),
+  (x) => x.e,
+);
+const part2 = minBy(
+  sortedNums.map((i) => ({ i, e: sumBy(nums, (n) => sumUntil(abs(n - i))) })),
+  (x) => x.e,
+);
+console.log(part1, part2);
---
+++ TypeScript/src/lib/math/sumUntil.ts
@@ -0,0 +1,3 @@
+export function sumUntil(n: number) {
+  return (n * (n + 1)) / 2;
+}
