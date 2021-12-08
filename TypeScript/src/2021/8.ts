9c9208dd4c569d80cb6b073f645d21f9afa96cc7
Andreas Roth <andreas.roth@esveo.com>
2021-12-08 06:43:11 +0100

    Add day 8 and prepare day 9

--- TypeScript/src/2021/8.ts
+++ TypeScript/src/2021/8.ts
@@ -1,15 +1,47 @@
-import getInput from '../lib/getInput'
+import { sumBy } from 'lodash';
+import getInput from '../lib/getInput';
+import permutations from '../lib/permutations';
 import { stringToLines } from '../lib/ts-it/lines';
-import { numbers } from '../lib/ts-it/numbers';
+import { sum } from '../lib/ts-it/sum';
 
 const input = getInput(8, 2021);
-const nums = numbers(input);
 const lines = stringToLines(input);
 
-let result
-for (const line of lines) {
-  console.log(line)
+const originalOrder = 'a b c d e f g'.split(' ');
+
+const validNums = [
+  'abcefg',
+  'cf',
+  'acdeg',
+  'acdfg',
+  'bcdf',
+  'abdfg',
+  'abdefg',
+  'acf',
+  'abcdefg',
+  'abcdfg',
+];
+
+const possiblePermutations = permutations(originalOrder);
+
+const outputs1 = lines.map((line) => {
+  const [input, output] = line.split(' | ').map((x) => x.split(' '));
+
+  const correctPermutation = possiblePermutations.find((permutation) => {
+    const mapped = input.map((num) => mapWires(num, permutation));
+    return mapped.every((num) => validNums.includes(num));
+  });
+
+  return output
+    .map((num) => mapWires(num, correctPermutation!))
+    .map((num) => validNums.indexOf(num));
+});
+console.log(sumBy(outputs1.flat(), (n) => ([1, 4, 7, 8].includes(n) ? 1 : 0)));
+console.log(sum(outputs1.map((n) => Number(n.join('')))));
+
+function mapWires(num: string, permutation: string[]): string {
+  return [...num]
+    .map((letter) => originalOrder[permutation.indexOf(letter)])
+    .sort()
+    .join('');
 }
-
-console.log(result)
-
