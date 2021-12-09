e4c72e1e61463409b263e047a544cf48e4fbaf63
Andreas Roth <andreas.roth@esveo.com>
2021-12-09 09:52:06 +0100

    Add 9 2021

---
+++ TypeScript/src/2021/10.ts
@@ -0,0 +1,15 @@
+import getInput from '../lib/getInput'
+import { stringToLines } from '../lib/ts-it/lines';
+import { numbers } from '../lib/ts-it/numbers';
+
+const input = getInput(10, 2021);
+const nums = numbers(input);
+const lines = stringToLines(input);
+
+let result
+for (const line of lines) {
+  console.log(line)
+}
+
+console.log(result)
+
--- TypeScript/src/2021/9.ts
+++ TypeScript/src/2021/9.ts
@@ -1,15 +1,48 @@
-import getInput from '../lib/getInput'
+import { sortBy, sumBy, uniqBy } from 'lodash';
+import getInput from '../lib/getInput';
+import { Vector } from '../lib/InfiniteGrid';
 import { stringToLines } from '../lib/ts-it/lines';
-import { numbers } from '../lib/ts-it/numbers';
 
 const input = getInput(9, 2021);
-const nums = numbers(input);
-const lines = stringToLines(input);
+const lines = stringToLines(input).map((l) => l.split('').map(Number));
+const cross = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1],
+];
 
-let result
-for (const line of lines) {
-  console.log(line)
+let lowPoints = [] as Vector[];
+for (let y = 0; y < lines.length; y++) {
+  for (let x = 0; x < lines[y].length; x++) {
+    if (
+      cross.every(([dx, dy]) => (lines[y + dy]?.[x + dx] ?? 10) > lines[y][x])
+    )
+      lowPoints.push([x, y]);
+  }
 }
 
-console.log(result)
+console.log(sumBy(lowPoints, ([x, y]) => lines[y][x] + 1));
 
+const basins = lowPoints.map((start) => ({ coords: [start] }));
+for (const basin of basins) {
+  for (let i = 0; i < basin.coords.length; i++) {
+    const [x, y] = basin.coords[i];
+    for (const [dx, dy] of cross) {
+      const neighbour = lines[y + dy]?.[x + dx] ?? 10;
+      if (neighbour < 9 && neighbour > lines[y][x]) {
+        basin.coords.push([x + dx, y + dy]);
+      }
+    }
+  }
+}
+
+const basinsWithUniqueCoords = basins.map(({ coords }) =>
+  uniqBy(coords, (c) => c.join('-')),
+);
+
+console.log(
+  sortBy(basinsWithUniqueCoords, (b) => -b.length)
+    .slice(0, 3)
+    .reduce((a, b) => a * b.length, 1),
+);
