19726e5948ad1c7e55989798b3931b3d1a5c3cef
Andreas Roth <andreas.roth@esveo.com>
2021-12-10 06:37:48 +0100

    Improve day 10

--- TypeScript/src/2021/10.ts
+++ TypeScript/src/2021/10.ts
@@ -1,50 +1,28 @@
-import { fromPairs, sortBy } from 'lodash';
+import { sortBy } from 'lodash';
 import getInput from '../lib/getInput';
-import { last } from '../lib/ts-it/last';
 import { stringToLines } from '../lib/ts-it/lines';
 
 const input = getInput(10, 2021);
 const lines = stringToLines(input);
 
-const pairs = fromPairs('[] () {} <>'.split(' ').map((pair) => pair.split('')));
-const open = Object.keys(pairs);
+const pairs = { '[': ']', '(': ')', '{': '}', '<': '>' };
 const pointsA = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
-
-function check(line: string) {
-  const stack: string[] = [];
-  for (const char of line) {
-    if (open.includes(char)) stack.push(char);
-    else if (pairs[last(stack)!] !== char) {
-      return { type: 'Error' as const, char };
-    } else {
-      stack.pop();
-    }
-  }
-  return { type: 'Incomplete' as const, stack };
-}
+const pointsB = { ')': 1, ']': 2, '}': 3, '>': 4 };
 
 let part1 = 0;
-const incompletes = lines.filter((line) => {
-  const result = check(line);
-  if (result.type === 'Error') {
-    part1 += pointsA[result.char];
-    return false;
-  }
-  return true;
-});
-console.log(part1);
-
-const pointsB = { ')': 1, ']': 2, '}': 3, '>': 4 };
-const scores = sortBy(
-  incompletes.map((line) => {
-    const result = check(line);
-    if (result.type === 'Error') throw new Error('wtf');
-    let score = 0;
-    for (const char of result.stack.reverse()) {
-      score *= 5;
-      score += pointsB[pairs[char]];
+let part2Scores: number[] = [];
+linesLoop: for (const line of lines) {
+  const stack: string[] = [];
+  for (const char of line) {
+    if (char in pairs) stack.push(char);
+    else if (char !== pairs[stack.pop()!]) {
+      part1 += pointsA[char];
+      continue linesLoop;
     }
-    return score;
-  }),
-);
-console.log(scores[Math.floor(scores.length / 2)]);
+  }
+  let score = 0;
+  for (const char of stack.reverse()) score = score * 5 + pointsB[pairs[char]];
+  part2Scores.push(score);
+}
+console.log(part1);
+console.log(sortBy(part2Scores)[Math.floor(part2Scores.length / 2)]);
