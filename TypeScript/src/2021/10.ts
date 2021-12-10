28f0f219419d300f837f6f3d3f1be9c557510afb
Andreas Roth <andreas.roth@esveo.com>
2021-12-10 06:23:15 +0100

    add 11 2021

--- TypeScript/src/2021/10.ts
+++ TypeScript/src/2021/10.ts
@@ -1,15 +1,50 @@
-import getInput from '../lib/getInput'
+import { fromPairs, sortBy } from 'lodash';
+import getInput from '../lib/getInput';
+import { last } from '../lib/ts-it/last';
 import { stringToLines } from '../lib/ts-it/lines';
-import { numbers } from '../lib/ts-it/numbers';
 
 const input = getInput(10, 2021);
-const nums = numbers(input);
 const lines = stringToLines(input);
 
-let result
-for (const line of lines) {
-  console.log(line)
+const pairs = fromPairs('[] () {} <>'.split(' ').map((pair) => pair.split('')));
+const open = Object.keys(pairs);
+const pointsA = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
+
+function check(line: string) {
+  const stack: string[] = [];
+  for (const char of line) {
+    if (open.includes(char)) stack.push(char);
+    else if (pairs[last(stack)!] !== char) {
+      return { type: 'Error' as const, char };
+    } else {
+      stack.pop();
+    }
+  }
+  return { type: 'Incomplete' as const, stack };
 }
 
-console.log(result)
+let part1 = 0;
+const incompletes = lines.filter((line) => {
+  const result = check(line);
+  if (result.type === 'Error') {
+    part1 += pointsA[result.char];
+    return false;
+  }
+  return true;
+});
+console.log(part1);
 
+const pointsB = { ')': 1, ']': 2, '}': 3, '>': 4 };
+const scores = sortBy(
+  incompletes.map((line) => {
+    const result = check(line);
+    if (result.type === 'Error') throw new Error('wtf');
+    let score = 0;
+    for (const char of result.stack.reverse()) {
+      score *= 5;
+      score += pointsB[pairs[char]];
+    }
+    return score;
+  }),
+);
+console.log(scores[Math.floor(scores.length / 2)]);
