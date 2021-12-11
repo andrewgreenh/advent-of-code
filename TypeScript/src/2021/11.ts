86bf71f2bae2d88e94f72f987f9b42f7d7052e23
Andreas Roth <andreas.roth@esveo.com>
2021-12-11 06:36:15 +0100

    Add 11 2021

--- TypeScript/src/2021/11.ts
+++ TypeScript/src/2021/11.ts
@@ -1,15 +1,41 @@
-import getInput from '../lib/getInput'
-import { stringToLines } from '../lib/ts-it/lines';
-import { numbers } from '../lib/ts-it/numbers';
+import getInput from '../lib/getInput';
+import { Vector } from '../lib/InfiniteGrid';
+import { squareAround } from '../lib/ts-it/squareCoords';
 
 const input = getInput(11, 2021);
-const nums = numbers(input);
-const lines = stringToLines(input);
+const grid = input.split('\n').map((line) => line.split('').map(Number));
 
-let result
-for (const line of lines) {
-  console.log(line)
+let flashCount = 0;
+
+for (let i = 0; true; i++) {
+  grid.forEach((row, y) => row.forEach((cell, x) => increment([x, y])));
+  const currentFlashCount = flashCount;
+
+  grid.forEach((row, y) =>
+    row.forEach((cell, x) => {
+      if (cell > 9) {
+        flashCount++;
+        grid[y][x] = 0;
+      }
+    }),
+  );
+
+  if (flashCount - currentFlashCount === 100) {
+    console.log(i + 1);
+    break;
+  }
+  if (i === 99) console.log(flashCount);
 }
 
-console.log(result)
+function increment([x, y]: Vector) {
+  grid[x][y]++;
 
+  if (grid[x][y] === 10) {
+    for (const [nx, ny] of squareAround([x, y], 3, false)) {
+      const neighbour = grid[ny]?.[nx];
+      if (neighbour === undefined) continue;
+
+      increment([nx, ny]);
+    }
+  }
+}
