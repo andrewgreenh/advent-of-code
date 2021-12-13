d799a915b3284cb56c00690ab022c2e5de5bca27
Andreas Roth <andreas.roth@esveo.com>
2021-12-13 06:27:19 +0100

    Add 13 2021

--- TypeScript/src/2021/13.ts
+++ TypeScript/src/2021/13.ts
@@ -1,15 +1,46 @@
-import getInput from '../lib/getInput'
-import { stringToLines } from '../lib/ts-it/lines';
+import { maxBy, sumBy } from 'lodash';
+import getInput from '../lib/getInput';
 import { numbers } from '../lib/ts-it/numbers';
+import { printGrid } from '../lib/ts-it/printGrid';
 
 const input = getInput(13, 2021);
-const nums = numbers(input);
-const lines = stringToLines(input);
+const [dataString, instructionString] = input.split('\n\n');
+const data = dataString.split('\n').map(numbers);
+let grid: string[][] = [];
 
-let result
-for (const line of lines) {
-  console.log(line)
+const maxX = maxBy(data, (n) => n[0])![0];
+const maxY = maxBy(data, (n) => n[1])![1];
+
+for (let x = 0; x <= maxX; x++) {
+  for (let y = 0; y <= maxY; y++) {
+    if (!grid[y]) grid[y] = [];
+    grid[y][x] = '.';
+  }
+}
+for (const [x, y] of data) {
+  grid[y][x] = '#';
 }
 
-console.log(result)
+const instructions = instructionString.split('\n').map((l) => {
+  const [, direction, num] = l.match(/(.)=(\d+)/)!;
+  return { direction, num: Number(num) };
+});
 
+let i = 0;
+for (const ins of instructions) {
+  let newGrid: typeof grid = [];
+  grid.forEach((row, y) =>
+    row.forEach((cell, x) => {
+      if (ins.direction === 'x' && x > ins.num) {
+        x = ins.num - (x - ins.num);
+      } else if (ins.direction === 'y' && y > ins.num) {
+        y = ins.num - (y - ins.num);
+      }
+      if (!newGrid[y]) newGrid[y] = [];
+      if (newGrid[y][x] !== '#') newGrid[y][x] = cell;
+    }),
+  );
+  grid = newGrid;
+  if (i++ === 0) console.log(sumBy(grid.flat(), (n) => (n === '#' ? 1 : 0)));
+}
+printGrid(grid, undefined, '');
