c27cf7ff8526035670a2cebe9e3d449d539dbca6
Andreas Roth <andreas.roth@esveo.com>
2021-12-16 06:46:45 +0100

    Add 16 2021

--- TypeScript/src/2021/16.ts
+++ TypeScript/src/2021/16.ts
@@ -1,15 +1,104 @@
-import getInput from '../lib/getInput'
-import { stringToLines } from '../lib/ts-it/lines';
-import { numbers } from '../lib/ts-it/numbers';
+import { max, min } from 'lodash';
+import getInput from '../lib/getInput';
+import { sum } from '../lib/ts-it/sum';
 
-const input = getInput(16, 2021);
-const nums = numbers(input);
-const lines = stringToLines(input);
+const input = [...getInput(16, 2021).trim()]
+  .map((c) => parseInt(c, 16).toString(2).padStart(4, '0'))
+  .join('');
 
-let result
-for (const line of lines) {
-  console.log(line)
+const ast = parsePacket(input);
+let part1 = 0;
+count(ast[0]);
+console.log(part1);
+console.log(evaluate(ast[0]));
+
+function count(node: Node) {
+  part1 += node.version;
+  if (node.type === 'OPERATOR') {
+    for (const n of node.nodes) count(n);
+  }
 }
 
-console.log(result)
+function evaluate(node: Node): number {
+  if (node.type === 'LITERAL') return node.value;
+  const childValues = node.nodes.map(evaluate);
+  const op = node.operatorId;
+  const opMap = {
+    0: sum,
+    1: (num: number[]) => num.reduce((a, b) => a * b),
+    2: min,
+    3: max,
+    5: ([a, b]: [number, number]) => (a > b ? 1 : 0),
+    6: ([a, b]: [number, number]) => (a < b ? 1 : 0),
+    7: ([a, b]: [number, number]) => (a === b ? 1 : 0),
+  };
+  return opMap[op](childValues);
+}
 
+type Node =
+  | {
+      version: number;
+      type: 'LITERAL';
+      value: number;
+    }
+  | {
+      version: number;
+      type: 'OPERATOR';
+      operatorId: number;
+      nodes: Node[];
+    };
+
+function parsePacket(rest: string): [Node, string] {
+  let taken = '';
+  [taken, rest] = take(3, rest);
+  const version = parseInt(taken, 2);
+  [taken, rest] = take(3, rest);
+  const typeId = parseInt(taken, 2);
+
+  if (typeId === 4) {
+    let num = '';
+    while (true) {
+      [taken, rest] = take(5, rest);
+      num += taken.slice(1);
+      if (taken[0] === '0') break;
+    }
+    const parsed = parseInt(num, 2);
+    return [{ type: 'LITERAL', value: parsed, version }, rest];
+  } else {
+    [taken, rest] = take(1, rest);
+    const lengthTypeId = taken;
+    const subNodes: Node[] = [];
+
+    if (lengthTypeId === '0') {
+      [taken, rest] = take(15, rest);
+      const lengthOfSubPackets = parseInt(taken, 2);
+      [taken, rest] = take(lengthOfSubPackets, rest);
+      let subPacketRest = taken;
+      while (subPacketRest.length) {
+        let packetNode;
+        [packetNode, subPacketRest] = parsePacket(subPacketRest);
+        subNodes.push(packetNode);
+      }
+    }
+    if (lengthTypeId === '1') {
+      [taken, rest] = take(11, rest);
+      let numberOfSubPackages = parseInt(taken, 2);
+      while (numberOfSubPackages--) {
+        let packetNode;
+        [packetNode, rest] = parsePacket(rest);
+        subNodes.push(packetNode);
+      }
+    }
+
+    return [
+      { type: 'OPERATOR', nodes: subNodes, version, operatorId: typeId },
+      rest,
+    ];
+  }
+}
+
+function take(n: number, rest: string) {
+  const taken = rest.slice(0, n);
+  const newRest = rest.slice(n);
+  return [taken, newRest];
+}
