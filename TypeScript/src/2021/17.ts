681d5e7fabd862c400411067b167ef178cc4f7b7
Andreas Roth <andreas.roth@esveo.com>
2021-12-17 06:31:13 +0100

    Add 17 2021

--- TypeScript/src/2021/17.ts
+++ TypeScript/src/2021/17.ts
@@ -1,15 +1,32 @@
-import getInput from '../lib/getInput'
-import { stringToLines } from '../lib/ts-it/lines';
+import getInput from '../lib/getInput';
 import { numbers } from '../lib/ts-it/numbers';
 
 const input = getInput(17, 2021);
-const nums = numbers(input);
-const lines = stringToLines(input);
+const [xFrom, xTo, yFrom, yTo] = numbers(input);
 
-let result
-for (const line of lines) {
-  console.log(line)
+let maxHeight = 0;
+const velocities = new Set<string>();
+
+for (let x = 1; x < 200; x++) {
+  for (let y = -yFrom; y > -200; y--) {
+    const height = checkAngle([x, y]);
+    if (height !== null) velocities.add(`${x},${y}`);
+    if (height && height > maxHeight) maxHeight = height;
+  }
 }
+console.log(maxHeight);
+console.log(velocities.size);
 
-console.log(result)
-
+function checkAngle([vx, vy]: [number, number]): number | null {
+  let [x, y] = [0, 0];
+  let maxY = 0;
+  while (x <= xTo && y >= yFrom) {
+    if (y > maxY) maxY = y;
+    if (x >= xFrom && x <= xTo && y >= yFrom && y <= yTo) return maxY;
+    x += vx;
+    y += vy;
+    vx -= Math.sign(vx);
+    vy--;
+  }
+  return null;
+}
