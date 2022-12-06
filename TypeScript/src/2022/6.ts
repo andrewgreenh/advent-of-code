7f6a079df399faa6c7f232723ffa02cef8541e1d
Andreas Roth <andreas.roth@esveo.com>
2022-12-06 06:11:48 +0100

    shorten 6 2022

--- TypeScript/src/2022/6.ts
+++ TypeScript/src/2022/6.ts
@@ -2,18 +2,11 @@ import getInput from '../lib/getInput';
 
 const input = getInput(6, 2022);
 
-for (let i = 0; i < input.length; i++) {
-  const marker = new Set(input.slice(i, i + 4));
-  if (marker.size === 4) {
-    console.log(i + 4);
-    break;
+function run(len: number) {
+  for (let i = len; i < input.length; i++) {
+    if (new Set(input.slice(i - len, i)).size === len) return i;
   }
 }
 
-for (let i = 0; i < input.length; i++) {
-  const marker = new Set(input.slice(i, i + 14));
-  if (marker.size === 14) {
-    console.log(i + 14);
-    break;
-  }
-}
+console.log(run(4));
+console.log(run(14));
